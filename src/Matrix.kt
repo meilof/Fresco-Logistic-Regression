744c2378860f286bc542aafc38f3eb95d9e80756
Spanbroek, Mark <mark@spanbroek.net>
2017-07-04 14:23:09 +0200

    Do not tolerate matrix with differing row sizes

--- src/Matrix.kt
+++ src/Matrix.kt
@@ -1,4 +1,16 @@
 class Matrix<T>(val elements: Array<Array<T>>) {
+    init {
+        var numberOfColumns: Int? = null
+        elements.forEach { element ->
+            if (numberOfColumns == null) {
+                numberOfColumns = element.size
+            }
+            if (numberOfColumns != element.size) {
+                throw IllegalArgumentException("Rows are not of equal size")
+            }
+        }
+    }
+
     fun element(row: Int, column: Int): T? {
         return elements[row][column]
     }
--- src/MatrixSpec.kt
+++ src/MatrixSpec.kt
@@ -1,6 +1,7 @@
 import com.winterbe.expekt.expect
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.it
+import org.junit.Assert.fail
 
 class MatrixSpec : Spek({
     val matrix = Matrix(arrayOf(
@@ -12,5 +13,17 @@ class MatrixSpec : Spek({
     it("can recall its elements") {
         expect(matrix.element(row = 0, column = 0)).to.equal(1.0)
         expect(matrix.element(row = 2, column = 1)).to.equal(6.0)
+    }
+
+    it("does not tolerate differing row sizes") {
+        try {
+            Matrix(arrayOf(
+                    arrayOf(1.0, 2.0),
+                    arrayOf(3.0, 4.0, 5.0)
+            ))
+            fail()
+        } catch (exception: IllegalArgumentException) {
+            // success
+        }
     }
 })
