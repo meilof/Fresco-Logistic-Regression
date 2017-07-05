f0665445a38df51f251d6c2511a4de446b4408e1
Spanbroek, Mark <mark@spanbroek.net>
2017-07-05 17:39:57 +0200

    Cleanup

--- src/MatrixSpec.kt
+++ src/MatrixSpec.kt
@@ -1,7 +1,6 @@
 import com.winterbe.expekt.expect
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.context
-import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
 import org.junit.Assert.fail
 
