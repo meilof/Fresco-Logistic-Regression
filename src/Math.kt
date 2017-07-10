969b6077283d0e4b56facdb37d8d336d459a130a
Stefan van den Oord <stefan.van.den.oord@philips.com>
2017-07-10 10:25:06 +0200

    Added recursive computation of exponential

---
+++ src/Math.kt
@@ -0,0 +1,13 @@
+
+val numIterations = 15
+
+fun exp(a: Double): Double {
+    return exp(a, 1)
+}
+
+fun exp(a: Double, iteration: Int): Double {
+    if (iteration > numIterations) { return a }
+
+    return 1.0 + a / iteration * exp(a, iteration + 1)
+    // optimization: precompute 1/2, 1/3, 1/4, ..., 1/15
+}
---
+++ src/MathSpec.kt
@@ -0,0 +1,9 @@
+import com.winterbe.expekt.expect
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.it
+
+class MathSpec: Spek({
+    it("can calculate exponential") {
+        expect(exp(5.0)).to.be.closeTo(Math.exp(5.0), 0.1)
+    }
+})
