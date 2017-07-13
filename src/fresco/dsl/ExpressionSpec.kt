10a571241272adc619a698b32ffbb994326e720a
Spanbroek, Mark <mark@spanbroek.net>
2017-07-13 14:15:50 +0200

    Added a test that shows combining known and closed integers in an expression

--- src/fresco/dsl/ExpressionSpec.kt
+++ src/fresco/dsl/ExpressionSpec.kt
@@ -6,25 +6,30 @@ import org.jetbrains.spek.api.dsl.it
 
 class ExpressionSpec : Spek({
 
-    val s1 = KnownInt(1)
-    val s2 = KnownInt(2)
-    val s3 = KnownInt(3)
+    val k1 = KnownInt(1)
+    val k2 = KnownInt(2)
+    val k3 = KnownInt(3)
+    val c4 = ClosedInt(4, 1)
 
     it("supports addition") {
-        expect(evaluate(s2 + s3)).to.equal(5)
+        expect(evaluate(k2 + k3)).to.equal(5)
     }
 
     it("supports subtraction") {
-        expect(evaluate(s3 - s2)).to.equal(1)
+        expect(evaluate(k3 - k2)).to.equal(1)
     }
 
     it("can chain expressions") {
-        expect(evaluate(s3 + s2 - s1)).to.equal(4)
-        expect(evaluate(s3 - s2 + s1)).to.equal(2)
+        expect(evaluate(k3 + k2 - k1)).to.equal(4)
+        expect(evaluate(k3 - k2 + k1)).to.equal(2)
     }
 
     it("supports multiplication") {
-        expect(evaluate(s2 * s3)).to.equal(6)
+        expect(evaluate(k2 * k3)).to.equal(6)
+    }
+
+    it("supports adding a known and a closed integer") {
+        expect(evaluate(k3 + c4)).to.equal(7)
     }
 })
 
