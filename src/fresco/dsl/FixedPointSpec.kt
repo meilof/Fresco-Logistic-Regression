e4e5d4af7bc89f71c0106858b64914aceaefde0e
Spanbroek, Mark <mark@spanbroek.net>
2017-07-13 15:55:59 +0200

    Support creating known and closed fixed point numbers

--- src/fresco/dsl/FixedPointSpec.kt
+++ src/fresco/dsl/FixedPointSpec.kt
@@ -1,16 +1,29 @@
 package fresco.dsl
 
 import com.winterbe.expekt.expect
+import dk.alexandra.fresco.framework.builder.ProtocolBuilderNumeric
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.it
+import org.mockito.Mockito
 
 class FixedPointSpec : Spek({
 
-    it("can be created") {
-        FixedPoint(1.23)
+    val value = 1.23
+    val party = 1
+
+    it("can create known fixed point number") {
+        val builder = Mockito.mock(ProtocolBuilderNumeric::class.java, Mockito.RETURNS_DEEP_STUBS)
+        KnownFixedPoint(value).build(builder)
+        Mockito.verify(builder.numeric()).known(value.toFixedPoint())
+    }
+
+    it("can create closed fixed point number") {
+        val builder = Mockito.mock(ProtocolBuilderNumeric::class.java, Mockito.RETURNS_DEEP_STUBS)
+        ClosedFixedPoint(value, party).build(builder)
+        Mockito.verify(builder.numeric()).input(value.toFixedPoint(), party)
     }
 
     it("can be evaluated") {
-        expect(evaluate(FixedPoint(1.23))).to.be.closeTo(1.23, delta = 0.01)
+        expect(evaluate(KnownFixedPoint(value))).to.be.closeTo(value, delta = 0.01)
     }
 })
--- src/fresco/dsl/KnownFixedPoint.kt
+++ src/fresco/dsl/KnownFixedPoint.kt
@@ -4,8 +4,14 @@ import dk.alexandra.fresco.framework.Computation
 import dk.alexandra.fresco.framework.builder.ProtocolBuilderNumeric
 import dk.alexandra.fresco.framework.value.SInt
 
-class FixedPoint(val value: Double) : FixedPointExpression {
+class KnownFixedPoint(val value: Double) : FixedPointExpression {
     override fun build(builder: ProtocolBuilderNumeric): Computation<SInt> {
         return builder.numeric().known(value.toFixedPoint())
+    }
+}
+
+class ClosedFixedPoint(val value: Double, val party: Int) : FixedPointExpression {
+    override fun build(builder: ProtocolBuilderNumeric): Computation<SInt> {
+        return builder.numeric().input(value.toFixedPoint(), party)
     }
 }
