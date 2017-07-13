3c3d1c13cd24578c4827f24e36f71f83f6a2ac56
Spanbroek, Mark <mark@spanbroek.net>
2017-07-13 11:23:59 +0200

    Check that a known Fresco secure int is created

--- src/fresco/dsl/SecureIntSpec.kt
+++ src/fresco/dsl/SecureIntSpec.kt
@@ -1,8 +1,11 @@
 package fresco.dsl
 
 import com.winterbe.expekt.expect
+import dk.alexandra.fresco.framework.builder.ProtocolBuilderNumeric
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.it
+import org.mockito.ArgumentMatchers
+import org.mockito.Mockito.*
 
 class SecureIntSpec : Spek({
 
@@ -12,6 +15,12 @@ class SecureIntSpec : Spek({
 
     it("can be evaluated as a Fresco computation") {
         expect(evaluate(s1)).to.equal(1)
+    }
+
+    it("creates a known Fresco SInt") {
+        val builder = mock(ProtocolBuilderNumeric::class.java, RETURNS_DEEP_STUBS)
+        s1.build(builder)
+        verify(builder.numeric()).known(ArgumentMatchers.any())
     }
 
     it("can be added to another secure int") {
